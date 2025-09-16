fbe0840163b9fcaffdae6ec018cfa7879079a789
KagChi <yonuxbot@gmail.com>
2025-09-16 21:57:11 +0700

    chore: update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard/Page";
 import { Route, Routes } from "react-router";
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+
+            <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
     );
 }
