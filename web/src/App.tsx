d0837b8591c1f12accd22300bc8808420cc8fb59
KagChi <yonuxbot@gmail.com>
2025-09-16 21:21:05 +0700

    chore: update theme

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import {Button} from "@/components/ui/button.tsx";
 
 export default function App() {
     return (
-        <main className="flex min-h-svh flex-col items-center justify-center bg-background">
+        <main className="flex min-h-svh flex-col items-center justify-center bg-background dark">
             <section className="w-full max-w-md p-8 bg-card rounded-lg shadow-md flex flex-col items-center">
                 <h1 className="text-3xl font-bold mb-4 text-primary">Welcome to the Home Page</h1>
                 <p className="mb-6 text-muted-foreground text-center">
